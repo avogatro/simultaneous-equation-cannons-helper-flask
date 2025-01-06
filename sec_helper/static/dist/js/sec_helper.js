8d6e7559ac68fd2c0c053f0b37fb9260994f7a32
avogatro <avogatro@gmail.com>
2025-01-06 17:50:51 +0100

    csp changes

--- sec_helper/static/dist/js/sec_helper.js
+++ sec_helper/static/dist/js/sec_helper.js
@@ -10,3 +10,11 @@ function changeSolution(send_xyz,send_fusion,returned_xyz,returned_fusion) {
     p.innerText = "Return Xyz "+ returned_xyz +" Fusion "+ returned_fusion;
     solutionInfo.appendChild(p);
 }
+document.querySelectorAll('.solution').forEach( (element) => {
+    element.style.color =element.dataset.color;
+});
+document.querySelectorAll('.solution-radio').forEach( (element) => {
+    element.addEventListener("change", function (event) {
+        changeSolution(element.dataset.sendxyz,element.dataset.sendfusion,element.dataset.returnedxyz,element.dataset.returnedfusion);
+    });
+});
