f00872537361c475673a090e76a20d63ead82e7e
avogatro <avogatro@gmail.com>
2026-06-11 18:34:57 +0200

    change url base

--- js/vite.config.js
+++ js/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SEC-Helper/',
+  base: '/simultaneous-equation-cannons-helper-flask/',
 })
